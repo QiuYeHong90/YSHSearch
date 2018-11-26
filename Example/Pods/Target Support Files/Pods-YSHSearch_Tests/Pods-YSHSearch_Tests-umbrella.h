01e90cbb8db915dde64bc58b5eb7b27f5d984f11
shuhui <793983383@qq.com>
2018-11-26 09:54:43 +0800

    更新
    更新

---
+++ Example/Pods/Target Support Files/Pods-YSHSearch_Tests/Pods-YSHSearch_Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_YSHSearch_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_YSHSearch_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/YSHSearch/YSHSearch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YSHSearch : NSObject
+@end
+@implementation PodsDummy_YSHSearch
+@end
